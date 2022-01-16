d357b2f905db122569e1b5b172307cb8b54f5038
Rayane Sousa <68711660+rayanesousa31@users.noreply.github.com>
2022-01-16 19:57:49 -0300

    Upload exercício 006 - Mundo 1

---
+++ exercício 006.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia um número e mostre seu dobro, triplo e raiz quadrada.
+
+num = int(input('Digite um número : '))
+
+print('O dobro do número {} é {}, seu triplo é {} e sua raiz quadrada é {}.'.format(num,(num*2),(num*3),(num**(1/2))))
