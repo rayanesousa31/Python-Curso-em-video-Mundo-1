5cd33eef5acf6e1ff4bfa3dc80f15b9d2669b9ef
Rayane Sousa <68711660+rayanesousa31@users.noreply.github.com>
2022-01-16 19:53:31 -0300

    Upload exercício 005 - Mundo 1

---
+++ exercicio 005.py
@@ -0,0 +1,7 @@
+#Faça um programa que leia um número inteiro e mostre na tela seu sucessor e seu antecessor.
+
+n1 = int(input('Digite o primeiro valor: '))
+
+print ( 'O antecessor do número {} é {} e o sucessor dele é {}'.format ( n1,(n1-1), (n1+1)))
+
+
